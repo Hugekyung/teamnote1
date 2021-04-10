540915c9453a719db78e83beb742f007776c4467
yang <kiki9510@gmail.com>
2021-04-11 03:25:46 +0900

    Added another method for maxHeap and solved 1655

--- Algorithm/heap/maxHeap.py
+++ Algorithm/heap/maxHeap.py
@@ -10,3 +10,9 @@ for num in nums:
 while heap:
     # heappop은 가장 작은 값을 출력하므로 -num이 작을 수록 실제 값은 가장 크다.
     print(heapq.heappop(heap)[1])  # index 1(값 출력)
+
+# 두번째 방법
+for num in nums:
+    heapq.heappush(heap, -num)
+for i in range(len(nums)):
+    print(-heapq.heappop(heap)) # -를 출력값 앞에 붙여서 -를 제거한다.
--- baekjoon/heap/middleValue1655.py
+++ baekjoon/heap/middleValue1655.py
@@ -1,12 +1,22 @@
 import sys
 import heapq
 
-n = int(input())
-heap = []
-for i in range(1, n+1):
-    x = int(input())
-    heapq.heappush(heap, x)
-    if i % 2 == 1: # 홀수이면
-        print(heap[round(i / 2) - 1])
-    else: # 짝수인 경우
-        print(heap[(i // 2) - 1])
+n = int(sys.stdin.readline())
+left_heap = [] # 최대 힙
+right_heap = [] # 최소 힙
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if len(left_heap) == len(right_heap):
+        heapq.heappush(left_heap, (-x, x))
+    else:
+        heapq.heappush(right_heap, x)
+
+    # left_heap의 top이 홀수일 경우는 중간값이 되고, 짝수인 경우에는 
+    # right_heap의 top과 대수비교를 해서 위치를 변경해야 한다.
+    # left_heap의 top이 right_heap의 top보다 크면 두 수의 위치를 바꾼다.
+    if right_heap and left_heap[0][1] > right_heap[0]:
+        tmp_left = heapq.heappop(left_heap)[1]
+        tmp_right = heapq.heappop(right_heap)
+        heapq.heappush(left_heap, (-tmp_right, tmp_right))
+        heapq.heappush(right_heap, tmp_left)
+    print(left_heap[0][1]) 
