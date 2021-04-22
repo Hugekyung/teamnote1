3f45441e7443395853880a532e5e2aa3d4f95897
yang <kiki9510@gmail.com>
2021-04-23 00:22:08 +0900

    Solved diameterOfTree1167.

--- baekjoon/tree/diameterOfTree1167.py
+++ baekjoon/tree/diameterOfTree1167.py
@@ -1,17 +1,41 @@
+from collections import deque
+import sys
+
+# 슬라이싱으로 접근하면 경우에 따라 값이 틀리는 경우가 발생하는 듯?
+# V = int(input())
+# tree = [[] for _ in range(V+1)]
+# for i in range(1, V+1):
+#     info = list(map(int, input().split()))[1:-1]
+#     for idx in range(0, len(info), 2):
+#         tree[i].append((info[idx], info[idx+1]))
+
 V = int(input())
 tree = [[] for _ in range(V+1)]
 for i in range(1, V+1):
-    info = list(map(int, input().split()))[1:-1]
-    for idx, v in enumerate(info):
-        if idx % 2 == 0:
-            tree[i].append((v, info[idx+1]))
+    info = list(map(int, input().split()))
+    for idx in range(1, len(info) - 2, 2):
+        tree[info[0]].append((info[idx], info[idx+1]))
 
-visited = [False] * (V+1)
+"""아무 노드에서 가장 멀리 있는 노드를 구하고,
+다시 그 노드에서부터 가장 멀리 있는 노드를 구한다."""
+def longest_distance(tree, start):
+    visited = [-1] * (V+1) # 방문처리와 동시에 해당 노드까지의 거리 저장!!
+    visited[start] = 0
+    dq = deque()
+    dq.append(start)
+    max_dt = [0, 0] # 거리, 정점
 
-def longest_distance(tree, visited):
-    visited[1] = True
-    distance = tree[1][1]
+    while dq:
+        node = dq.popleft()
+        for i, dt in tree[node]:
+            if visited[i] == -1:
+                visited[i] = visited[node] + dt
+                dq.append(i)
+                if max_dt[0] < visited[i]:
+                    max_dt = visited[i], i
 
-    for i in tree[1:]:
-        for d in i:
-        if visited[i[0]]
+    return max_dt
+
+distance, node = longest_distance(tree, 1)
+distance, node = longest_distance(tree, node)
+print(distance)
--- baekjoon/tree/temp.py
+++
@@ -1,4 +0,0 @@
-x = [i for i in range(5)]
-info = x[1:-1]
-for idx, val in enumerate(info):
-    print(idx, val)
