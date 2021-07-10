a5885a460669dffb0b5bdc70ce3e69c22e548d12
yang <kiki9510@gmail.com>
2021-07-10 23:34:20 +0900

    Solved threesum9095.

--- baekjoon/DP/threesum9095_N.py
+++ baekjoon/DP/threesum9095_N.py
@@ -7,3 +7,12 @@ for _ in range(t):
     dp = [0,1,0,0]
     for i in range(1, n):
         pass
+
+
+t = int(input())
+ott = [1, 2, 4]
+for i in range(3, 10):
+    ott.append(ott[i - 3] + ott[i - 2] + ott[i - 1])
+for i in range(t):
+    n = int(input())
+    print(ott[n - 1])
